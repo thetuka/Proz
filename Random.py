09d570c28a259852423f4aeea9180f08f65c6736
Denise <torres_486@hotmail.com>
2024-04-23 22:55:42 -0300

    New Archive

---
+++ Random.py
@@ -0,0 +1,9 @@
+#Crie uma função que gere um número aleatório entre 1 e 10. 
+# O usuário deve ser perguntado para inserir um valor. 
+# Enquanto o usúario não acertar o valor, a função deve continuar perguntado o valor. 
+# Ao acertar, a função deve imprimir uma mensagem ao usuário o valor correto e o número de tentativas até o acerto. 
+# Caso o valor inserido não seja um número inteiro a função deve lançar uma mensagem de erro.
+
+def random(numeros):
+    num_aleatorio = random(1, 10)
+    
